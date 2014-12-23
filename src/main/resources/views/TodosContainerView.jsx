423b7172c2521db6673612c1700d370e5e16fc9b
cavenator <cavenator@gmail.com>
2014-12-22 23:30:14 -0800

    Revised to include one-jar bundling and initial refactor to clean up / design right

--- src/main/resources/views/TodosContainerView.jsx
+++ src/main/resources/views/TodosContainerView.jsx
@@ -5,86 +5,58 @@ define(["jquery", "underscore","react", "jsx!views/CommentList","lib/bootstrap/j
    var Button = React.createClass({
       render: function(){
          var propsToOmit = _.omit(this.props, ["className","onClick","label"])
-         return <button type="button" className={this.props.classes} onClick={this.props.onClick}>{this.props.label}</button>;
-      }
-   });
-   var Modal = React.createClass({
-      render: function(){
-          var children = this.props.children, transformedChildren = [];
-          var transform = function(obj){
-             return <div className={obj.className}>{obj.child}</div>
-          };
-          transformedChildren.push(transform({className: "modal-body", child: children[0]}));
-          transformedChildren.push(transform({className: "modal-footer", child: children[1]}));
-         return (<div className="modal fade" id={this.props.id} tabIndex="-1" role="dialog" aria-hidden="true">
-                 <div className="modal-header"><Button classes="close" label="x"></Button><h4>{this.props.title}</h4></div>
-                    { transformedChildren }
-                </div>);
-      }
-   });
-   var ModalSection = React.createClass({
-      render: function(){
-          var modalClasses = "modal-"+this.props.type;
-          if (this.props.classes){
-             modalClasses += " " + this.props.classes;
-          }
-
-          return <div className={modalClasses}>{ this.props.children }</div>;
+         return <button type="button" className={this.props.className} onClick={this.props.onClick} style={this.props.style}>{this.props.label}</button>;
       }
    });
 
-   var ModalMixin = {
-       setModalId: function(id){
-          this.modalId = id;
-       },
-       showModal: function(){
-          var modal = document.getElementById(this.modalId);
-          $(modal).modal('show');
-       },
-       hideModal: function(){
-          var modal = document.getElementById(this.modalId);
-          $(modal).modal('hide');
-       }
-   };
+		var TodosList = React.createClass({
+			defaultProps: {
+				data: []
+			},
+			createTodosFrom: function(data){
+				return (
+						<div>
+							<span>Title</span><span>{data.title}</span><br />
+							<span>Description</span><span>{data.description}</span>
+						</div>
+					);
+			},
+			render: function(){
+				var mappedChildren = _.map(this.props.data, function(data){ return this.createTodosFrom(data) }, this);
+				return (
+					<div className="row-fluid">
+						<div className="span2" />
+						{mappedChildren}
+						<div className="span2" />
+					</div>
+				);
+			}
+		});
 
-   var CommentBox = React.createClass({
-      mixins: [ModalMixin],
-      getInitialState: function(){
-          return { data: [] };
-      },
-      loadTodos: function(){
-         $.ajax({
-            url: this.props.url,
-            dataType: "json",
-            success: function(data){
-               this.setState({data: data});
-            }.bind(this)
-         });
-      },
-      componentDidMount: function(){
-          this.loadTodos();
-          if (this.props.pollingInterval){
-            setInterval(this.loadTodos, this.props.pollingInterval);
-          }
-      },
-      render: function(){
-          var modalId = "myModal";
-          this.setModalId(modalId);
-          return (<div id="commentBox">
-                      <h1>{this.props.headline}</h1>
-                      <CommentList data={this.state.data} />
-                      <Modal id={modalId} title="I am the title of the modal">
-                          <div>I am the body</div>
-                          <div>I am the footer <Button classes="btn btn-inverse" onClick={this.hideModal} label="Close" /></div>
-                      </Modal>
-                      <Button classes="btn btn-info" onClick={this.showModal} label="Click me for a modal" />
-                  </div>);
-      }
-   });
+		var TodosButtonList = React.createClass({
+			render: function(){
+				var floatRight = {
+					float: 'right'
+				};
+				return (
+					<div className="row-fluid">
+						<div className="span3" />
+						<div className="span3"><Button label="Add" className="btn btn-primary" style={floatRight} /></div>
+						<div className="span3"><Button label="Clear all" className="btn" /></div>
+						<div className="span3" />
+					</div>
+				);
+			}
+		});
 
     var TodosContainerView = React.createClass({
        render: function(){
-           return <CommentBox headline="The main Comments box" url="/todo" />;
+           return (
+										<div>
+											<TodosButtonList />
+											<TodosList url="/todo" />
+										</div>
+					);
        }
 
     });
