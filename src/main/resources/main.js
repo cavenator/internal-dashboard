f6b04807470fb0e8a022c80fb1d0a7a51ab2d823
AJ Cave <acave@cj.com>
2015-07-15 06:45:10 -0700

    Adding amcharts

--- src/main/resources/main.js
+++ src/main/resources/main.js
@@ -2,6 +2,7 @@ require.config({
    paths: {
      "jquery"      : "https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min",
      "bootstrap"   : "lib/bootstrap/js/bootstrap",
+     "amcharts"    : "lib/amcharts/amcharts",
      "underscore"  : "lib/underscore",
      "react"       : "lib/react/react",
      "JSXTransformer": "lib/react/JSXTransformer",
